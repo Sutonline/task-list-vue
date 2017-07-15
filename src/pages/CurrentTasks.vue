3b8b911a8b2da3908f7e8aa9664827b77ef1e292
yongkang.zhang <yongkang.zhang@7fresh.com>
2017-07-15 16:25:33 +0800

    :memo: 增加doc

--- src/pages/CurrentTasks.vue
+++ src/pages/CurrentTasks.vue
@@ -41,20 +41,23 @@
                :visible.sync="dialogVisible"
                size="tiny"
                :before-close="handleClose">
-      <el-form ref="newTask" :model="form" label-width="80px">
-        <el-form-item label="任务类别">
-          <el-select v-model="form.region" placeholder="请选择">
-            <el-option label="区域二" value="shanghai"></el-option>
-            <el-option label="区域一" value="beijing"></el-option>
+      <el-form ref="newTask" :model="form" :rules="ruleValidate" label-width="80px">
+        <el-form-item label="任务类别" prop="label">
+          <el-select v-model="form.label" placeholder="请选择">
+            <el-option v-for="item in labels"
+                       :key="item.name"
+                       :label="item.value"
+                       :value="item.name">
+            </el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="任务内容">
+        <el-form-item label="任务内容" prop="content">
           <el-input type="textarea" :rows="2" placeholder="请输入内容" v-model="form.content"></el-input>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确定</el-button>
+        <el-button type="primary" @click="addNewTask('newTask')">确定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -68,9 +71,18 @@
     data () {
       return {
         form: {
-          region: '',
+          label: '',
           content: ''
         },
+        ruleValidate: {
+          label: [{
+            required: true,
+            message: '请选择标签'
+          }],
+          content: [{
+            required: true,
+            message: '任务内容必填'}]},
+        labels: [],
         dialogVisible: false,
         tableData: [
           {
@@ -91,6 +103,12 @@
         ]
       }
     },
+    route: {
+      data: function (transition) {
+        // var q = this.$route.query // 获取url的路由查询对象
+        this.loadData()
+      }
+    },
     methods: {
       delete: function (scope) {
         console.log(scope)
@@ -99,7 +117,8 @@
         console.log(scope)
       },
       loadData: function () {
-
+        this.getAllLabel()
+        this.getNonFinishedTask()
       },
       // 每行className设置
       setRowClassName (row, index) {
@@ -109,7 +128,7 @@
         this.$confirm('确认关闭?')
           .then(_ => {
             done()
-          }).catch(_ => {})
+          }).catch()
       },
       getNonFinishedTask: function () {
         this.$http.get(api.GET_NON_FINISH_TASK).then(
@@ -117,6 +136,54 @@
             console.log(res)
           }
         )
+        this.getAllLabel()
+      },
+      getAllLabel: function () {
+        this.$http.get(api.QUERY_LABELS).then(
+          res => {
+            this.labels = res.data
+          }
+        )
+      },
+      doneTask: function (taskId) {
+        this.$http.post(api.DONE + '/?id=' + taskId).then(
+          res => {
+            console.log(res)
+          }
+        )
+      },
+      newTask: function (formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            const label = this.form.label
+            const content = this.form.content
+            this.$http.post(api.SAVE, {
+              id: 0,
+              label: label,
+              content: content
+            }).then(
+                res => {
+                  this.$message(res)
+                }
+            ).catch(
+              error => {
+                this.$message('提交错误' + error)
+              }
+            )
+            console.log()
+          } else {
+            console.log('error submit')
+            return false
+          }
+        })
+          /* this.$http.post(api.SAVE, task).then(
+          res => {
+            console.log(res)
+          }
+        ) */
+      },
+      addNewTask: function (task) {
+        this.newTask(task)
       }
     }
   }
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,15 +1,17 @@
 <template>
-  <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm login-container">
+  <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px"
+           class="demo-ruleForm login-container">
     <h3 class="title">系统登录</h3>
     <el-form-item prop="account">
-      <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
+      <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="Kevin?"></el-input>
     </el-form-item>
     <el-form-item prop="checkPass">
-      <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
+      <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="请输入密码"></el-input>
     </el-form-item>
     <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
     <el-form-item style="width:100%;">
-      <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录</el-button>
+      <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录
+      </el-button>
       <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
     </el-form-item>
   </el-form>
@@ -17,21 +19,23 @@
 
 <script>
   // import NProgress from 'nprogress'
+  import * as api from '../api/task-list-api'
+
   export default {
     data () {
       return {
         logining: false,
         ruleForm2: {
-          account: 'admin',
-          checkPass: '123456'
+          account: '',
+          checkPass: ''
         },
         rules2: {
           account: [
-            { required: true, message: '请输入账号', trigger: 'blur' }
+            {required: true, message: '请输入账号', trigger: 'blur'}
             // { validator: validaePass }
           ],
           checkPass: [
-            { required: true, message: '请输入密码', trigger: 'blur' }
+            {required: true, message: '请输入密码', trigger: 'blur'}
             // { validator: validaePass2 }
           ]
         },
@@ -39,7 +43,9 @@
       }
     },
     methods: {
-
+      checkLogin: function () {
+        this.$http.post(api.DONE).then({})
+      }
     }
   }
 </script>
