dbc5dfea6d92c54b96f5056b115179dca58b918f
yongkang.zhang <yongkang.zhang@7fresh.com>
2017-10-10 20:14:59 +0800

    增加备案信息

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -4,6 +4,13 @@
     <transition name="slide-fade">
       <router-view class="content"></router-view>
     </transition>
+
+    <!-- 增加备案信息 -->
+    <el-row>
+      <el-col span="24" style="text-align:center;align-items: center">
+        <a href="http://www.miitbeian.gov.cn/">京ICP备17056815号</a>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
